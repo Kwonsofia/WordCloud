3f7e3ade854a172cd0d18f59a76adad2469b28e5
Kwonsofia <hyemin0723@swu.ac.kr>
2022-12-05 23:55:53 +0900

    MOD: search API 추가

--- API/views.py
+++ API/views.py
@@ -1,1 +1,21 @@
+from app import app
+from flask import request, jsonify
 from service.utils import google_crawler, naver_crawler
+
+
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask'
+
+
+@app.route('/search', method=['GET'])
+def search_word():
+    try:
+        word = request.args['word']
+    except Exception as ex:
+        return jsonify(ex)
+
+    google_img_url = google_crawler(word)
+    naver_img_url = naver_crawler(word)
+
+    return True
